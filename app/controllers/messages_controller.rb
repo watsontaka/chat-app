22fbde9edbf4182f0655d5130dde13ed59d134c9
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-08-01 12:45:05 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,9 @@
 class MessagesController < ApplicationController
+  
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.inculde(:user)
   end
 
   def create
@@ -10,6 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redilect_to room_messages_path(@room)
     else
+      @messages = @room.messages.inculde(:user)
       render :index, status: :unprocessable_entity
   end
 
